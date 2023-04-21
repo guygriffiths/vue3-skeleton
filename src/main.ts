99b3ad4ddf7e3ef7361e620c4619bf8ff00c19f6
Guy G <guy.griffiths@juggler.net>
2023-04-21 10:26:14 +0100

    A few small updates

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import Cookies from 'js-cookie'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -11,18 +10,6 @@ const app = createApp(App)
 const pinia = createPinia()
 app.use(pinia)
 app.use(router)
-
-const cookieName = import.meta.env.X_KEYCLOAK_LOGIN_COOKIE_NAME
-function createCookie(token: string) {
-	// Create our cookie with token from keycloak object
-	Cookies.set(cookieName, token, {
-		domain: import.meta.env.X_KEYCLOAK_LOGIN_COOKIE_DOMAIN,
-		path: '/',
-		expires: 1,
-		secure: cookieName.startsWith('__Secure-'),
-	})
-}
-
 
 const store = useStore()
 // TODO use language store here (if required)
